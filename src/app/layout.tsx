d0c7519356ba98a415dd1e7bc97b3c89ced14046
ethancarter7 <ethancarter72005@gmail.com>
2025-07-07 11:11:40 -0600

    Fixed padding on mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body>
         <Navbar />
-        <main style={{ paddingTop: '5rem' }}>{children}</main>
+        <main className="main-content">{children}</main>
         <Footer />
         <Analytics />
         <SpeedInsights />
